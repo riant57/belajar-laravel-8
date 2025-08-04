374ee37ce55f4434dc8a8d7f35137adc50357375
riant57 <riant57@gmail.com>
2025-08-05 06:01:04 +0700

    belajar Model, Collection dan Controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('blog',[
+            "title" => "Blog",
+            "posts" => Post::all(),
+        ]);
+    }
+
+    public function show($slug)
+    {
+        return view('blog-single',[
+            "title" => "Blog",
+            "post"  => Post::find($slug),
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,54 +32,5 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title"  => "Post Pertama",
-            "slug"   => "post-pertama",
-            "author" => "Dimas",
-            "body"   => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores architecto cupiditate iste natus velit dolores ab consectetur ullam odit repellendus excepturi cum doloribus"
-        ],
-        [
-            "title"  => "Post Kedua",
-            "slug"   => "post-kedua",
-            "author" => "Bagus",
-            "body"   => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores architecto cupiditate iste natus velit dolores ab consectetur ullam odit repellendus excepturi cum doloribus, minima, asperiores tempora aliquam, expedita obcaecati. Eaque!"
-        ],
-
-    ];
-    return view('blog',[
-        "title" => "Blog",
-        "posts" => $blog_posts
-    ]);
-});
-
-Route::get('/blog/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title"  => "Post Pertama",
-            "slug"   => "post-pertama",
-            "author" => "Dimas",
-            "body"   => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores architecto cupiditate iste natus velit dolores ab consectetur ullam odit repellendus excepturi cum doloribus"
-        ],
-        [
-            "title"  => "Post Kedua",
-            "slug"   => "post-kedua",
-            "author" => "Bagus",
-            "body"   => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores architecto cupiditate iste natus velit dolores ab consectetur ullam odit repellendus excepturi cum doloribus, minima, asperiores tempora aliquam, expedita obcaecati. Eaque!"
-        ],
-
-    ];
-    $single_post = [];
-    foreach($blog_posts as $post){
-        if($post['slug'] === $slug){
-            $single_post = $post;
-        }
-    }
-    //dd($single_post);
-    
-    return view('blog-single',[
-        "title" => "Blog",
-        "post"  => $single_post
-    ]);
-});
+Route::get('/blog', [PostController::class,'index']);
+Route::get('/blog/{slug}', [PostController::class,'show'] );
